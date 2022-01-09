0a5e1ad2c6e0fc4e1e026fd732a7f0669dbc6a6a
Rafaeliandos <94220815+Rafaeliandos@users.noreply.github.com>
2022-01-09 19:56:25 +0700

    Create No 2.cpp

---
+++ No 2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string adalahPalindrome(string S)
+{
+    for (int i = 0; i < S.length() / 2; i++) {
+            return "No";
+        }
+    }
+    return "Yes";
+}
+int main()
+{
+    string S = "ABCDCBA";
+    cout << adalahPalindrome(S);
+ 
+    return 0;
